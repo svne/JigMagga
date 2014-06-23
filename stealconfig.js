29b57e9dc80504b7bd32902a34d3b4d5a506bf61
Jens Naie <naie@lieferando.de>
2014-06-23 19:22:55 +0200

    JigMagga update dependencies

--- stealconfig.js
+++ stealconfig.js
@@ -2,7 +2,7 @@ steal.config({
     jmENV : "development",
     map: {
         "*": {
-            "jquery/jquery.js": (this.navigator && this.navigator.userAgent.indexOf("MSIE") !== -1) || (typeof global !== "undefined" && global.DEFAULTS.browser && DEFAULTS.browser.indexOf("msie") !== -1) ? "bower_components/jquery-legacy/jquery.js" :  "bower_components/jquery/jquery.min.js",
+            "jquery/jquery.js": (this.navigator && this.navigator.userAgent.indexOf("MSIE") !== -1) || (typeof global !== "undefined" && global.DEFAULTS.browser && DEFAULTS.browser.indexOf("msie") !== -1) ? "bower_components/jquery/jquery.js" :  "bower_components/jquery2/jquery.min.js",
             "jquery/jstorage": "bower_components/jstorage"
         }
     },

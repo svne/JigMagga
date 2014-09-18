bf179704650554027cf65c2d2caf63cca87372d9
jhony-chikens <evts85@gmail.com>
2014-09-18 11:59:00 +0200

    return back push restaurant to the queue script

---
+++ worker/scripts/pushRestaurantsToRabbit.js
@@ -0,0 +1,41 @@
+#! /usr/local/bin/node
+'use strict';
+
+var amqp = require('amqp');
+
+var config = require('../config');
+var fixtures = require('../testData/restaurants.json');
+var amqpPublishOptions = {
+    contentType: 'text/plain',
+    deliveryMode: 1
+};
+
+var amqpConnection = amqp.createConnection(config.amqp.credentials);
+
+var publishMessages = function (exchange, queue) {
+    console.log('amount of records is ', fixtures.length);
+
+    fixtures.forEach(function (message) {
+        exchange.publish(queue, message, amqpPublishOptions);
+    });
+};
+console.log('start');
+amqpConnection.on('ready', function () {
+    console.log("Connected to RabbitMQ");
+    var exc = amqpConnection.exchange("amq.direct", {type: "direct"});
+
+    exc.on('open', function () {
+        console.log('exchange is open');
+
+        amqpConnection.queue('pages.generate.high', {durable: true, autoDelete: false}, function (queue) {
+            queue.bind('amq.direct', 'pages.generate.high');
+            console.log('queue obtained');
+            publishMessages(exc, 'pages.generate.high');
+            process.exit();
+        });
+    });
+});
+
+amqpConnection.on('error', function (err) {
+    console.log('amqp error', err);
+});

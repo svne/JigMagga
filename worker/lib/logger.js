b809429f44239333251d095101fabdbdfa5380c4
jhony-chikens <evts85@gmail.com>
2014-09-19 15:36:27 +0200

    turn of file logger

--- worker/lib/logger.js
+++ worker/lib/logger.js
@@ -14,7 +14,7 @@ var colors = _.assign(winston.config.cli.colors, config.main.logger.colors);
 /**
  * returns a log function for current component
  * metadata if passed is added to each log message
- * 
+ *
  * @param  {string} component
  * @param  {object} metadata
  * @return {function}
@@ -22,15 +22,15 @@ var colors = _.assign(winston.config.cli.colors, config.main.logger.colors);
 module.exports = function (component, metadata) {
     metadata = metadata || {};
 
-    var filename = path.join(__dirname, '../..',  config.main.logger.folder, component + '.log');
+    var filename = path.join(__dirname, '../..',  config.main.logger.folder, component + '_' + process.pid + '.log');
     var options = config.main.logger.file;
     options.filename = filename;
 
     var logger = new (winston.Logger)({
         levels: logLevels,
         transports: [
-            new (winston.transports.Console)(config.main.logger.console),
-            new (winston.transports.File)(options)
+            new (winston.transports.Console)(config.main.logger.console)
+            // new (winston.transports.File)(options)
         ]
     });
 
@@ -66,13 +66,13 @@ module.exports = function (component, metadata) {
      *
      * but if you want to insert an object in to string you have to add some additional empty object
      * in order to prevent the usage of your object like a metadata
-     * 
+     *
      * @example
      * //wrong
      * log('foo: %j', {a: 1}) // 'foo %j', metadata = {a: 1}
      * //correct
      * log('foo: %j', {a: 1}, {}) // 'foo {a: 1}', metadata = {}
-     * 
+     *
      * function will do nothing in test environment in order to not print anything while tests are running
      */
     return function () {

0a445694ff1d9e12be5276ef867eada70dc6f253
jhony-chikens <evts85@gmail.com>
2014-11-03 16:43:29 +0100

    worker: -  use rsyslog as a transport

--- worker/lib/logger.js
+++ worker/lib/logger.js
@@ -4,12 +4,49 @@ var _ = require('lodash');
 var winston = require('winston');
 var config = require('../config');
 
-require('winston-loggly');
+require('winston-rsyslog');
+/**
+ * capitalize first letter
+ * @param {String} word
+ * @return {String}
+ */
+var capitalFirst = function (word) {
+    return  word.charAt(0).toUpperCase() + word.substr(1, word.length);
+};
 //extend default winston log levels with custom
 var logLevels = _.assign(winston.config.cli.levels, config.main.logger.customLevels);
 
 //extend default winston colors with custom
 var colors = _.assign(winston.config.cli.colors, config.main.logger.colors);
+
+var loggers = {};
+
+var getLogger = function (component, config) {
+    if (loggers[component]) {
+        return loggers[component];
+    }
+
+    var logger = new (winston.Logger)({
+        levels: logLevels,
+        transports: [
+            new (winston.transports.Console)(config.console)
+        ]
+    });
+
+    if (config.transports && _.isObject(config.transports)) {
+        _.each(config.transports, function (options, transportName) {
+            var className = capitalFirst(transportName);
+
+            if (winston.transports[className]) {
+                logger.add(winston.transports[className], options);
+            }
+        });
+    }
+
+    loggers[component] = logger;
+
+    return logger;
+};
 
 /**
  * returns a log function for current component
@@ -22,24 +59,11 @@ var colors = _.assign(winston.config.cli.colors, config.main.logger.colors);
 module.exports = function (component, metadata) {
     metadata = metadata || {};
 
-    var filename = path.join(__dirname, '../..',  config.main.logger.folder, 'error_' + process.pid + '.log');
-    var options = config.main.logger.file;
-    options.filename = filename;
-    options.level = 'error';
     if (_.contains(process.argv, '-v') || _.contains(process.argv, '--verbose')) {
         config.main.logger.console.level = 'info';
     }
 
-    var logger = new (winston.Logger)({
-        levels: logLevels,
-        transports: [
-            new (winston.transports.Console)(config.main.logger.console)
-        ]
-    });
-
-    //if (config.main.logger.loggly) {
-    //    logger.add(winston.transports.Loggly, config.main.logger.loggly);
-    //}
+    var logger = getLogger(component, config.main.logger);
 
     winston.addColors(colors);
 

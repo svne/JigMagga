svne/JigMagga

commit 51ffc58e0218dcf586ea8aae0c68ab3b7a4099e0

refactor(yd.build.index): Script fails when live enviroment is not set
- Added function to fail script when live enviroment is not set
FED-410 15m

build/index.js
@@ -58,7 +58,8 @@ if (program.uploadmedia) {
  */
 
 else  {
-
+    checkBucketEnv();
+    
     helper.createStreamWithSettings(program)
         .pipe(configMerger.getConfig())
         .pipe(configMerger.getPagesThatMatchThePageParam())
@@ -90,5 +91,12 @@ process.on('uncaughtException', function (err) {
     process.stdout.write('error :' + err + err.stack);
     process.kill();
 });
+
+function checkBucketEnv(){
+    if(program.bucket.indexOf('stage') !== -1 && !program.live) {
+        throw new Error('-x --live option is required');
+        return false;
+    }
+}
 
 

commit 5b180891b5868423e5f7762251be76f14255593e

fix(yd.build.index): Invert logic for bucket misconfiguration detection
51ffc58 introduced a wrong logic to detect whether someone is trying
to deploy stage files into production

build/index.js
@@ -59,7 +59,7 @@ if (program.uploadmedia) {
 
 else  {
     checkBucketEnv();
-    
+
     helper.createStreamWithSettings(program)
         .pipe(configMerger.getConfig())
         .pipe(configMerger.getPagesThatMatchThePageParam())
@@ -93,7 +93,9 @@ process.on('uncaughtException', function (err) {
 });
 
 function checkBucketEnv(){
-    if(program.bucket.indexOf('stage') !== -1 && !program.live) {
+    var isLiveBucket = program.bucket.indexOf('stage') === -1;
+
+    if(isLiveBucket && !program.live) {
         throw new Error('-x --live option is required');
         return false;
     }
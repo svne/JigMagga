0c7576b5c8f57f3fa08fda1965e5755bdaf770bd
Stephan Schneider <stephanschndr@gmail.com>
2016-03-09 15:41:38 +0100

    refactor(worker/logger): only create one main logger
    - clean up structure
    - derive child logger from only one parent

--- worker/lib/logger.js
+++ worker/lib/logger.js
@@ -2,13 +2,84 @@
 
 var _ = require('lodash'),
     gelfStream = require('gelf-stream'),
-    konphyg = require('konphyg'),
     bunyan = require('bunyan'),
     bunyanLogstash = require('bunyan-logstash'),
-    path = require('path'),
     config = require('../config'),
-    program = require('../parseArguments')(process.argv);
+    parseArguments = require('../parseArguments');
 
+var program = parseArguments(process.argv);
+var logLevels = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
+
+var logger;
+var graylogConfig;
+var graylogTransport;
+
+
+/*
+    An example of the graylog setting in the config file
+
+        logger: {
+            "graylog": {
+                 "host": "graylog-domain.com",
+                 "port": 12345
+            }
+        }
+ */
+
+function getGraylogConfig() {
+    if (program.graylogHost && program.graylogPort) {
+        return {
+            host: program.graylogHost,
+            port: program.graylogPort
+        };
+    }
+
+    if (config.main.logger.graylog) {
+        return config.main.logger.graylog;
+    }
+
+    return null;
+}
+
+function graylogLogger(graylogConf) {
+    var graylog = gelfStream.forBunyan(graylogConf.host, graylogConf.port);
+    return {
+        type: 'raw',
+        stream: graylog,
+        level: 'info'
+    };
+}
+
+logger = bunyan.createLogger({
+    name: config.main.logger.name || 'htmlWorker',
+    streams: [
+        {
+            level: config.main.logger.defaultLogLevel,
+            type: 'raw',
+            stream: bunyanLogstash.createStream({
+                host: config.main.logger.logstash.host,
+                port: config.main.logger.logstash.port
+            })
+        },
+        {
+            stream: process.stdout,
+            level: 'debug'
+        }
+    ]
+});
+
+graylogConfig = getGraylogConfig();
+if (graylogConfig) {
+    graylogTransport = graylogLogger(graylogConfig);
+
+    // GELF stream has to be explicitly ended
+    process.on('exit', function () {
+        logger.info('Exiting... %s', process.pid);
+        graylogTransport.stream.end();
+    });
+
+    logger.addStream(graylogTransport);
+}
 
 /**
  * Logger works with stdout, graylog or/and logstash.
@@ -25,6 +96,7 @@ var _ = require('lodash'),
  * @returns {Function}
  */
 module.exports = function (component, metadata, processArguments) {
+    var childLogger;
 
     metadata = metadata || {};
     processArguments = processArguments || {};
@@ -34,104 +106,21 @@ module.exports = function (component, metadata, processArguments) {
         metadata.tag = processArguments.tag;
     }
 
-    // bunyan log levels
-    var logLevels = ['trace', 'debug', 'info', 'warn', 'error', 'fatal'];
+    childLogger = logger.child({metadata: metadata});
 
-    // wanna use graylog?
-    var graylogConfig = {
-        "active": true,
-        "host": null,
-        "port": null
-    };
-
-    // graylog settings from the command line
-    if (program.graylogHost && program.graylogPort) {
-
-        graylogConfig.host = program.graylogHost;
-        graylogConfig.port = program.graylogPort;
-
-    } else if (config.main.logger.graylog) {
-
-        // graylog settings from config file
-        /*
-            An example of the graylog setting in the config file
-
-                logger: {
-                    "graylog": {
-                         "host": "graylog-domain.com",
-                         "port": 12345
-                    }
-                }
-         */
-
-        graylogConfig.host = config.main.logger.graylog.host;
-        graylogConfig.port = config.main.logger.graylog.port;
-
-    } else {
-        graylogConfig.active = false;
-    }
-
-    var logger = bunyan.createLogger({
-            name:    config.main.logger.name || 'htmlWorker',
-            streams: [
-                {
-                    level: config.main.logger.defaultLogLevel,
-                    type: 'raw',
-                    stream: bunyanLogstash.createStream({
-                        host: config.main.logger.logstash.host,
-                        port: config.main.logger.logstash.port
-                    })
-                }
-                ,
-                {
-                    stream: process.stdout,
-                    level:  'debug'
-                }
-            ]
-        })
-        .child({metadata: metadata});
-
-    // add graylog stream
-    if (graylogConfig.active) {
-        var graylog = gelfStream.forBunyan(graylogConfig.host, graylogConfig.port);
-        var graylogStream = {
-            type: 'raw',
-            stream: graylog,
-            level: 'info'
-        };
-        logger.addStream(graylogStream);
-    }
-
-    // GELF stream has to be explicitly ended
-    process.on('exit', function () {
-        logger.error('Exiting...');
-        if (graylogConfig.active) {
-            graylog.end();
-        }
-    });
-
-    // Catches ctrl+c event
-    process.on('SIGINT', function () {
-        var errorMessage = 'Interupted...';
-
-        logger.error(errorMessage);
-        return setTimeout(function () {
-            throw new Error(errorMessage);
-        }, 100);
-    });
-
-    // if using standard the standard bunyan logger (without logger factory) uncomment the next statement
-    //return logger;
+    // if using standard the standard bunyan logger (without logger factory)
+    // uncomment the next statement
+    // return logger;
 
     // factory for the old logging style
-    return function loggerFactory () {
+    return function loggerFactory() {
+        var args = Array.prototype.slice.apply(arguments);
+        var meta = args.pop();
+        var func;
 
         if (process.env.NODE_ENV === 'silent') {
             return;
         }
-
-        var args = Array.prototype.slice.apply(arguments);
-        var meta = args.pop();
 
         if (_.isPlainObject(meta)) {
             meta = _.assign(meta, metadata);
@@ -141,14 +130,14 @@ module.exports = function (component, metadata, processArguments) {
             args.push(metadata);
         }
 
-        // will be called
-        var func;
-        if (logLevels.indexOf(args[0]) !== -1) {        // log level is known
+        if (logLevels.indexOf(args[0]) !== -1) {
+            // log level is known
             func = args.shift();
         } else {
-            func = config.main.logger.defaultLogLevel;  // unknown log level
+            // unknown log level
+            func = config.main.logger.defaultLogLevel;
         }
 
-        logger[func].apply(logger, args);
+        childLogger[func].apply(logger, args);
     };
 };
